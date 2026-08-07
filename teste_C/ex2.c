a76e7f0378a70ce9567edb6ab88588e9d807e99f
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2026-08-07 15:01:37 -0300

    Add files via upload

---
+++ teste_C/ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    double x = 25.0;
+    double raiz = sqrt(x);
+    printf("Raiz quadrada de %.2f: %.2f\n", x, raiz); //errado
+    
+
+
+    return 0;
+
+}
