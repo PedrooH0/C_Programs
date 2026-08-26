0ea261cedcc0b7c21d862637b975a5f9ab68ed7a
PedrooH0 <pedrofarias210120@gmail.com>
2026-08-26 11:53:31 -0300

    criação do for que deixa todos os armários ocupados

--- desafio_01C/code01.c
+++ desafio_01C/code01.c
@@ -1,40 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <math.h>
 
 int main(){
 
+  int i;
+  int sum = 0;
   unsigned char ch;
   unsigned short int rd;
-  do{
-    printf("Selecione uma das seguintes opções:\n1. Ocupar armário\n2. Liberar armário\n3. Sair\n");
-    scanf("%hhu",&ch);
+  unsigned char control;
 
-    switch (ch)
-    {
-    case 1:
-      puts("você está no 1");
-      //ch=0;
+  for(i=0;i<=7;i++){ // for que deixa todos os armários ocupados
+    if (i==0) sum = sum + 1;
+    sum = sum + (1<<i);
+    printf("%d\n",sum);
+  } 
+  puts("");
+  
 
-      rd = rand() % 8;
-      printf("%hu\n\n",rd);
+  
+  // do{
+  //   printf("Selecione uma das seguintes opções:\n1. Ocupar armário\n2. Liberar armário\n3. Sair\n");
+  //   scanf("%hhu",&ch);
 
-      break;
+  //   switch (ch)
+  //   {
+  //   case 1:
+  //     puts("você está no 1");
+  //     //ch=0;
 
-    case 2:
-      puts("você está no 2\n");
-      //ch=0;
-      break;
+  //     rd = rand() % 8;
+  //     printf("%hu\n\n",rd);
 
-    case 3:
-      puts("Adeus");
-      ch=0;
-      break;
+  //     break;
+
+  //   case 2:
+  //     puts("você está no 2\n");
+  //     //ch=0;
+  //     break;
+
+  //   case 3:
+  //     puts("Adeus");
+  //     ch=0;
+  //     break;
     
-    default:
-      puts("Nenhuma opção selecionada, tente novamete");
-      break;
-    }
-  }while (ch != 0);
+  //   default:
+  //     puts("Nenhuma opção selecionada, tente novamete");
+  //     break;
+  //   }
+  // }while (ch != 0);
   
   return 0;
 
