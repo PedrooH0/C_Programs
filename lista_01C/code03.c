d96b11e230c3de834695c16cbc0eaa5331bfd242
PedrooH0 <pedrofarias210120@gmail.com>
2026-08-29 22:08:54 -0300

    adição da questão 03

--- lista_01C/code03.c
+++ lista_01C/code03.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
   int num;
   puts("Escreva um número:");
   scanf("%i", &num);
-  printf("\nValor original: %i\nTriplo: %i\nQuadrado: %i\nMeio: %i",num,3*num,pow(num,2),num/2);
+  printf("\nValor original: %i\nTriplo: %i\nQuadrado: %i\nMeio: %i",num,3*num,num*num,num/2);
 
   return 0;
 }
