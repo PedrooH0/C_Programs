dd13a2a61e1b99e6510e7d336144e22d18b8e2ff
PedrooH0 <pedrofarias210120@gmail.com>
2026-09-04 17:43:07 -0300

    questão 14 imcompleta

---
+++ lista_01C/code14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+  unsigned int diasTrabalhados;
+  float salario;
+
+  puts("Informe a sua quantidade de dias trabalhados: ");
+  scanf("%u",&diasTrabalhados);
+
+  if((diasTrabalhados>10)){
+
+  }
+
+  return 0;
+}
