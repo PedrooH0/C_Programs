c8b7aeb87cfc2054735d71dc93931c63840abdb2
PedrooH0 <pedrofarias210120@gmail.com>
2026-09-18 16:35:29 -0300

    adição questão 14

--- lista_01C/code14.c
+++ lista_01C/code14.c
@@ -3,14 +3,25 @@
 int main(){
 
   unsigned int diasTrabalhados;
-  float salario;
+  float salarioBruto, salarioLiquido;
 
   puts("Informe a sua quantidade de dias trabalhados: ");
   scanf("%u",&diasTrabalhados);
 
-  if((diasTrabalhados>10)){
 
+  salarioBruto = 50.25 * diasTrabalhados;
+
+  if((diasTrabalhados>10) && (diasTrabalhados<=20)){
+    salarioLiquido = salarioBruto * 1.2;
   }
+
+  if(diasTrabalhados>20){
+    salarioLiquido = salarioBruto * 1.3;
+  }
+
+  salarioLiquido = salarioLiquido * 0.9;
+
+  printf("Seu salário líquido é de %.2f",salarioLiquido);
 
   return 0;
 }
