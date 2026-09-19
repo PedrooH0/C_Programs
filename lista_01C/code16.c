4a74df6cca0573ead279623740c9fdc5318b9a78
PedrooH0 <pedrofarias210120@gmail.com>
2026-09-19 16:10:43 -0300

    adição da questão 16

---
+++ lista_01C/code16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int num,mod;
+
+  puts("Insira um número: ");
+  scanf("%d",&num);
+
+  mod = abs(num);
+
+  printf("O módulo de %d é: %d\n",num,mod);
+}
