4116489db1fdc9adcdd8b4fab154d16c65e5d46d
PedrooH0 <pedrofarias210120@gmail.com>
2026-09-19 22:14:49 -0300

    adição da questão 21

---
+++ lista_01C/code21.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+  char x = 65, y = 66, z = 67, a = 97, b = 98, c = 99, p = 36, q = 43,r = 47,s = 32;
+
+  printf("%c %c %c\n%c %c %c\n%c %c %c\n%c", x,y,z,a,b,c,p,q,r,s);
+
+  return 0;
+}
